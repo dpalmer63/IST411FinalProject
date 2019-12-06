ebab899d6d615848da451bbf06fc1b49764359f4
Daniel Palmer <danielpalmer@Daniels-MacBook-Pro-4.local>
2019-12-06 12:23:41 -0500

    A couple changes

--- Server/server.js
+++ Server/server.js
@@ -6,21 +6,27 @@ const port = 3000;
 //var cors = require('cors');
 //app.use(cors());
 
-let resList = [
+let apiList = [
   {
-    image: 'product1',
+    image: 'https://www.lions-pride.com/assets/product_images/3894.jpg',
     name: 'Penn State College of IST T-Shirt',
     price: '20.00'
   },
   {
-    image: 'product2',
+    image: 'https://images.nittanyweb.com/scs/images/products/15/original/penn_state_university_smeal_college_of_business_t_shirt_nittany_lions_psu_p9118.jpg',
     name: 'Penn State University Smeal College of Business T-Shirt',
     price: '13.49'
   }
 ];
-fs.writeFile('server.json', JSON.stringify(resList), err => {
+fs.writeFile('server.json', JSON.stringify(apiList), err => {
   if (err) throw err;
   console.log('Saved file');
+});
+
+app.get('/getImg', (req, res) => {
+  let apiData = fs.readFileSync("server.json");
+  let apiInfo = JSON.parse(apiData);
+
 });
 
 app.listen(port, () => {
