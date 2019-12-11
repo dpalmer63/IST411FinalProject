5a5d00b87f0538da2b02019feaaaad32e1aad502
muffiser <49293582+muffiser@users.noreply.github.com>
2019-12-10 23:30:33 -0500

    search

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,7 @@ let apiList = [
     price: '20.00'
   },
   {
-    src: 'https://images.nittanyweb.com/scs/images/products/15/original/penn_state_university_smeal_college_of_business_t_shirt_nittany_lions_psu_p9118.jpg',
+    src: 'https://images.nittanyweb.com/scs/images/products/15/origintermal/penn_state_university_smeal_college_of_business_t_shirt_nittany_lions_psu_p9118.jpg',
     name: 'Penn State University Smeal College of Business T-Shirt',
     price: '13.49'
   }
@@ -24,29 +24,11 @@ fs.writeFile('server.json', JSON.stringify(apiList), err => {
   console.log('Saved file');
 });
 
-app.get('/getAll', (req, res) => {
+app.get('/getImg', (req, res) => {
   let apiData = fs.readFileSync("server.json");
   let apiInfo = JSON.parse(apiData);
 
-  res.send(apiInfo);
-});
-
-app.post('/source/:src/name/:name/price/:price', (req, res) => {
-  let src = req.params.src;
-  let name = req.params.name;
-  let price = req.params.price;
-
-  let newItem = {};
-  newItem.src = src;
-  newItem.name = name;
-  newItem.price = price;
-
-  apiList.push(newItem);
-
-  fs.writeFile('server.json', JSON.stringify(apiList), err => {
-    if (err) throw err;
-    console.log('Saved file');
-  });
+  res.send("test");
 });
 
 app.listen(port, () => {
