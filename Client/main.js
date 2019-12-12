ace286a00f51ae42d7a0fe595118d9bc9b817c93
Daniel Palmer <danielpalmer@Daniels-MacBook-Pro-4.local>
2019-12-11 19:49:04 -0500

    Centered everything

--- Client/main.js
+++ Client/main.js
@@ -11,20 +11,22 @@
 
       for (var i = 0; i < data.length; i++)
       {
-        let div = document.createElement("div");
-        rowDiv.setAttribute('class', 'col-sm-2');
-        document.querySelector("#content").appendChild(div);
-
         let img = document.createElement("img");
         img.setAttribute("src", data[i].src);
-        img.setAttribute("width", "100");
-        img.setAttribute("height", "100");
+        img.setAttribute("width", "200");
+        img.setAttribute("height", "200");
         document.querySelector("#content").appendChild(img);
 
-        //let h3 = document.createElement("p");
-        //let text = document.createTextNode(data[i].name);
-        //h3.appendChild(text);
-        //document.querySelector("#content").appendChild(h3);
+        let pricetxt = document.createElement("p");
+        let text = document.createTextNode("$" + data[i].price);
+        pricetxt.appendChild(text);
+        document.querySelector("#content").appendChild(pricetxt);
+
+        let title = document.createElement("p");
+        let ptext = document.createTextNode(data[i].name);
+        title.setAttribute("style", "font-weight:bold");
+        title.appendChild(ptext);
+        document.querySelector("#content").appendChild(title);
 
       }
     }
